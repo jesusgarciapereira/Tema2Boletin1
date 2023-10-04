3bef6aac642e7c780aa33360f53d99bddbf0b821
jgarcia <jgarcia@106-13.info2>
2023-10-04 09:35:09 +0200

    hecho el 5

--- Tema2Boletin1/src/tema2/Ejercicio04.java
+++ Tema2Boletin1/src/tema2/Ejercicio04.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Ejercicio04 {
 
 	public static void main(String[] args) {
-		// Declaramos las variables num1 y num2, para ordenarlos de mayor a menor
+		// Declaramos las variables num1 y num2, para ordenarlos de menor a mayor
 		double num1, num2;
 		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
@@ -16,20 +16,25 @@ public class Ejercicio04 {
 		System.out.println("Introduzca un número");
 		// Leemos el número
 		num1 = sc.nextDouble();
-		// Le pedimos al usuario un número
+		// Le pedimos al usuario otro número
 		System.out.println("Introduzca otro número");
 		// Leemos el número
 		num2 = sc.nextDouble();
-		// Si el primer número es mayor que el segundo, me los mostrará en el mismo orden
-		if (num1 > num2) {
-			System.out.println("Ambos números ordenados de mayor a menor serían " + num1 + " y " + num2);
+		/*
+		 * Si el primer número es menor que el segundo, me los mostrará en el mismo
+		 * orden
+		 */
+		if (num1 < num2) {
+			System.out.println("Ambos números ordenados de menor a mayor serían " + num1 + " y " + num2 + "\n" + num1
+					+ " < " + num2);
 			// Si ambos números son iguales, me los mostrará tal cual
 		} else if (num1 == num2) {
 			System.out.println("En este caso, ambos números son iguales\n" + num1 + " = " + num2);
 		}
-		// Si el primer número es menor que el segundo, me los mostrará en orden inverso
+		// Si el primer número es mayor que el segundo, me los mostrará en orden inverso
 		else {
-			System.out.println("Ambos números ordenados de mayor a menor serían " + num2 + " y " + num1);
+			System.out.println("Ambos números ordenados de menor a mayor serían " + num2 + " y " + num1 + "\n" + num2
+					+ " < " + num1);
 		}
 		// Cerramos el Scanner
 		sc.close();
---
+++ Tema2Boletin1/src/tema2/Ejercicio05.java
@@ -0,0 +1,48 @@
+package tema2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ejercicio05 {
+
+	public static void main(String[] args) {
+		// Declaramos las variables num1, num2 y num3, para ordenarlos de mayor a menor
+		double num1, num2, num3;
+		// Activamos el Scanner
+		Scanner sc = new Scanner(System.in);
+		// Para poder escribir en la consola decimales con punto
+		sc.useLocale(Locale.US);
+		// Le pedimos al usuario un número
+		System.out.println("Introduzca un número");
+		// Leemos el número
+		num1 = sc.nextDouble();
+		// Le pedimos al usuario otro número
+		System.out.println("Introduzca otro número");
+		// Leemos el número
+		num2 = sc.nextDouble();
+		// Le pedimos al usuario otro número
+		System.out.println("Introduzca otro número");
+		// Leemos el número
+		num3 = sc.nextDouble();
+		// Mostramos la primera parte del resultado
+				System.out.print("Los números ordenados de mayor a menor serían ");
+				// La segunda parte del resultado dependerá de las siguientes condiciones
+		if (num1 >= num2 && num2 >= num3) {
+			System.out.println(num1 + ", " + num2 + " y " + num3);
+		} else if (num1 >= num3 && num3 > num2) {
+			System.out.println(num1 + ", " + num3 + " y " + num2);
+		} else if (num2 > num1 && num1 >= num3) {
+			System.out.println(num2 + ", " + num1 + " y " + num3);
+		} else if (num2 >= num3 && num3 > num1) {
+			System.out.println(num2 + ", " + num3 + " y " + num1);
+		} else if (num3 > num1 && num1 >= num2) {
+			System.out.println(num3 + ", " + num1 + " y " + num2);
+		} else {
+			System.out.println(num3 + ", " + num2 + " y " + num1);
+		}
+		// Cerramos el Scanner
+		sc.close();
+	}
+	
+
+}
