93e6673eccbd900467321c7185490fd4c7c0913c
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-10-05 12:33:41 +0200

    hasta el 7

--- Tema2Boletin1/src/tema2/Ejercicio06.java
+++ Tema2Boletin1/src/tema2/Ejercicio06.java
@@ -13,9 +13,10 @@ public class Ejercicio06 {
 		double a, b, c;
 		/*
 		 * Declaramos la variable x1 y x2; serán las dos posibles soluciones de nuestra
-		 * ecuación
+		 * ecuación. Creo también x0 que es para casos en los que no sea una ecuación de
+		 * segundo grado
 		 */
-		double x1, x2;
+		double x1, x2, x0;
 		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
 		// Para poder escribir en la consola decimales con punto
@@ -36,16 +37,24 @@ public class Ejercicio06 {
 		c = sc.nextDouble();
 		// Mostramos cómo quedaría la ecuación con las variables que hemos pedido
 		System.out.println("Así queda la ecuación: " + a + "x^2 + " + b + "x + " + c + " = 0");
-		// Establecemos la relación entre las variables
-		x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
-		x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
 		// Mostramos la primera parte del resultado
 		System.out.print("La solución es: ");
-		// Mostramos el resultado de la ecuación, descartando los casos en los que x no
-		// sea un número real
-		if (Math.sqrt(Math.pow(b, 2) - 4 * a * c) < 0) {
+		// Mostramos el resultado de la ecuación, en caso de que x no sea un número real
+		if ((Math.pow(b, 2) - 4 * a * c) < 0) {
 			System.out.println("En este caso x no es un número real");
-		} else {System.out.println("x = " + x1 + ", x = " + x2);}
+		}
+		// En caso de que no sea una ecuación de segundo grado
+		else if (a == 0) {
+			x0 = -c / b;
+			System.out.println("x = " + x0);
+		}
+		// En los demás casos, ecuación de segundo grado con dos incógnitas
+		else {
+			x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
+			x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
+			System.out.println("x = " + x1 + ", x = " + x2);
+		}
+		// Cerramos el Scanner
+		sc.close();
 	}
-
 }
---
+++ Tema2Boletin1/src/tema2/Ejercicio07.java
@@ -0,0 +1,31 @@
+package tema2;
+
+import java.util.Scanner;
+
+public class Ejercicio07 {
+
+	public static void main(String[] args) {
+		// Declaramos las variables numero, para calcular el número de cifras que tiene
+		int numero;
+		// Activamos el Scanner
+		Scanner sc = new Scanner(System.in);
+		// Le pedimos al usuario un número
+		System.out.println("Introduzca un número del 0 al 99999");
+		// Leemos el número
+		numero = sc.nextInt();
+		if (numero < 0 || numero > 99999) {
+			System.out.println("Número no valido, debe estar comprendido entre 0 y 99999");
+		} else if (numero >= 0 && numero <= 9) {
+			System.out.println("El número " + numero + " tiene una cifra");
+		} else if (numero >= 10 && numero <= 99) {
+			System.out.println("El número " + numero + " tiene dos cifras");
+		} else if (numero >= 100 && numero <= 999) {
+			System.out.println("El número " + numero + " tiene tres cifras");
+		} else if (numero >= 1000 && numero <= 9999) {
+			System.out.println("El número " + numero + " tiene cuatro cifras");
+		} else {
+			System.out.println("El número " + numero + " tiene cinco cifras");
+
+		}
+	}
+}
