d72301ad33beb7cdcdd4fb96b51c23319899f9f8
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-10-06 13:34:32 +0200

    terminado sin comentar

---
+++ Tema2Boletin1/src/tema2/Ejercicio09.java
@@ -0,0 +1,34 @@
+package tema2;
+
+import java.util.Scanner;
+
+public class Ejercicio09 {
+
+	public static void main(String[] args) {
+		String jugador1, jugador2;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Vamos a jugar a PIEDRA, PAPEL, TIJERA");
+		System.out.println("Jugador 1, elige una opción (en mayúsculas)");
+		jugador1 = sc.nextLine();
+		System.out.println("Jugador 2, elige una opción (en mayúsculas)");
+		jugador2 = sc.nextLine();
+		System.out.println("El jugador 1 ha elegido: " + jugador1 + "\nEl jugador 2 ha elegido: " + jugador2);
+		if (jugador1.equals("PIEDRA") && jugador2.equals("PIEDRA")
+				|| jugador1.equals("PAPEL") && jugador2.equals("PAPEL")
+				|| jugador1.equals("TIJERAS") && jugador2.equals("TIJERAS")) {
+			System.out.println("Empate");
+		} else if (jugador1.equals("PIEDRA") && jugador2.equals("TIJERAS")
+				|| jugador1.equals("PAPEL") && jugador2.equals("PIEDRA")
+				|| jugador1.equals("TIJERAS") && jugador2.equals("PAPEL")) {
+			System.out.println("Gana el jugador 1");
+		} else if (jugador1.equals("PIEDRA") && jugador2.equals("PAPEL")
+				|| jugador1.equals("PAPEL") && jugador2.equals("TIJERAS")
+				|| jugador1.equals("TIJERAS") && jugador2.equals("PIEDRA")) {
+			System.out.println("Gana el jugador 2");
+		} else {
+			System.out.println("Opcion incorrecta");
+		}
+		sc.close();
+	}
+
+}
---
+++ Tema2Boletin1/src/tema2/Ejercicio10.java
@@ -0,0 +1,31 @@
+package tema2;
+
+import java.util.Scanner;
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		int num1, num2, num3;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Escribe un número entero");
+		num1 = sc.nextInt();
+		System.out.println("Escribe otro");
+		num2 = sc.nextInt();
+		System.out.println("Escribe otro");
+		num3 = sc.nextInt();
+		if (num1 + num2 == num3 || num1 + num3 == num2 || num2 + num3 == num1) {
+			System.out.print("La suma de dos de los números nos da el tercero: ");
+			if (num1 + num2 == num3) {
+				System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+			} else if (num1 + num3 == num2) {
+				System.out.println(num1 + " + " + num3 + " = " + (num1 + num3));
+			} else if (num2 + num3 == num1) {
+				System.out.println(num2 + " + " + num3 + " = " + (num2 + num3));
+			}
+		} else {
+			System.out.println("No hay ninguna relación entre estos números");
+		}
+		sc.close();
+	}
+
+}
