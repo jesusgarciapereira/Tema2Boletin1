332276e5fe22f88a642c697c33195d845b81db83
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-10-13 14:27:13 +0200

    el 7

---
+++ Tema2Boletin1/src/boletin3/Ejercicio07.java
@@ -0,0 +1,32 @@
+package boletin3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio07 {
+
+	public static void main(String[] args) {
+		int numElegido, numAleatorio;
+		Random r = new Random();
+		numAleatorio = r.nextInt(1, 101);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("He pensado un número del 1 al 100, adivínalo");
+		System.out.println("Si quieres rendirte, elige -1");
+		numElegido = sc.nextInt();
+
+		while (numElegido != numAleatorio && numElegido >= 0) {
+			if (numAleatorio < numElegido) {
+				System.out.println("Menos");
+			} else {
+				System.out.println("Más");
+			}
+			numElegido = sc.nextInt();
+		}
+		if (numElegido == -1) {
+			System.out.println("Qué pena... había pensado en el " + numAleatorio);
+		} else if (numElegido == numAleatorio) {
+			System.out.println("¡Correcto! ¿Jugamos otra vez?");
+		}
+		sc.close();
+	}
+}
