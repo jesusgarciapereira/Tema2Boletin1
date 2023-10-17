2cf5a7edad11f83eb054bf907062a44b0fb57f14
Jesús <Jess@LAPTOP-8MILMTUM>
2023-10-17 23:59:27 +0200

    el 5 tiene tela...

--- Tema2Boletin1/src/boletin4/Ejercicio05.java
+++ Tema2Boletin1/src/boletin4/Ejercicio05.java
@@ -1,10 +1,31 @@
 package boletin4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ejercicio05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int numPensado;
+		String acierto;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Piensa un número del 1 al 100 y trataré de adivinarlo");
+		System.out.println("Luego me indicas si tu número es 'mayor', 'menor' o 'igual'");
+		System.out.println("Pulsa INTRO para comenzar");
+		sc.nextLine();
+		Random r = new Random();
+		numPensado = r.nextInt(1, 101);
+		do {
+			System.out.println("¿Es el " + numPensado + "?");
+			acierto = sc.nextLine();
+				if (acierto.equalsIgnoreCase("mayor")) {
+					numPensado = r.nextInt(numPensado + 1, 101);
+				} else if (acierto.equalsIgnoreCase("menor")) {
+					numPensado = r.nextInt(1, numPensado);
+				} else if (!acierto.equalsIgnoreCase("igual"))
+				System.out.println("Recuerda, escribe 'mayor', 'menor' o 'igual'");
+		} while (!acierto.equalsIgnoreCase("igual"));
+		System.out.println("¡Qué bien!");
+		sc.close();
 	}
-
 }
