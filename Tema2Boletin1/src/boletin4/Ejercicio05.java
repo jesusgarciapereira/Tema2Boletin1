02a385ee50b174c8958db7d3c1ff69ee9edf7eeb
Jesús <Jess@LAPTOP-8MILMTUM>
2023-10-18 00:41:25 +0200

    por fin el 5

--- Tema2Boletin1/src/boletin4/Ejercicio05.java
+++ Tema2Boletin1/src/boletin4/Ejercicio05.java
@@ -8,20 +8,23 @@ public class Ejercicio05 {
 	public static void main(String[] args) {
 		int numPensado;
 		String acierto;
+		int min = 1; 
+		int max = 101;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Piensa un número del 1 al 100 y trataré de adivinarlo");
 		System.out.println("Luego me indicas si tu número es 'mayor', 'menor' o 'igual'");
 		System.out.println("Pulsa INTRO para comenzar");
 		sc.nextLine();
 		Random r = new Random();
-		numPensado = r.nextInt(1, 101);
+		
 		do {
+			numPensado = r.nextInt(min, max);
 			System.out.println("¿Es el " + numPensado + "?");
 			acierto = sc.nextLine();
 				if (acierto.equalsIgnoreCase("mayor")) {
-					numPensado = r.nextInt(numPensado + 1, 101);
+					min = numPensado;
 				} else if (acierto.equalsIgnoreCase("menor")) {
-					numPensado = r.nextInt(1, numPensado);
+					max = numPensado;
 				} else if (!acierto.equalsIgnoreCase("igual"))
 				System.out.println("Recuerda, escribe 'mayor', 'menor' o 'igual'");
 		} while (!acierto.equalsIgnoreCase("igual"));
