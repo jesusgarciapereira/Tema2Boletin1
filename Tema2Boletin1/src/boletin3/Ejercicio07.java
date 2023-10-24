5f2db42cdb6f391b19b260670877d9fa4050c688
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-10-24 12:48:52 +0200

    clase

--- Tema2Boletin1/src/boletin3/Ejercicio07.java
+++ Tema2Boletin1/src/boletin3/Ejercicio07.java
@@ -6,22 +6,38 @@ import java.util.Scanner;
 public class Ejercicio07 {
 
 	public static void main(String[] args) {
+		/*
+		 * Declaramos la variable numElegido para el número que escribira el usuario y
+		 * la variable numAleatorio para el número aleatorio que elegirá el programa
+		 */
 		int numElegido, numAleatorio;
+		// Creamos un objeto de la clase Random
 		Random r = new Random();
+		/*
+		 * Asignamos a la variable numAleatorio un nuevo número entre 1 y 100 generado
+		 * aleatoriamente
+		 */
 		numAleatorio = r.nextInt(1, 101);
+		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
+		// Explicamos el juego
 		System.out.println("He pensado un número del 1 al 100, adivínalo");
 		System.out.println("Si quieres rendirte, elige -1");
+		// Le pedimos un número al usuario
 		numElegido = sc.nextInt();
+		// Seguirá preguntando números mientras no acierte y no se rinda
 		while (numElegido != -1 && numElegido != numAleatorio) {
+			
 			if (numElegido >= 1 && numElegido <= 100) {
+				// Si el número aleatorio es menor que mi número elegido 
 				if (numAleatorio < numElegido) {
-					System.out.println("Menos");
+					System.out.println("Menos, introduzca otro número");
 				} else {
-					System.out.println("Más");
+					System.out.println("Más, introduzca otro número");
 				}
 			} else {
 				System.out.println("Recuerda: un número del 1 al 100 o ríndete eligiendo -1");
+				System.out.println("Vuelve a introducir un número");
 			}
 			numElegido = sc.nextInt();
 		}
--- Tema2Boletin1/src/boletin4/Ejercicio05.java
+++ Tema2Boletin1/src/boletin4/Ejercicio05.java
@@ -6,29 +6,56 @@ import java.util.Scanner;
 public class Ejercicio05 {
 
 	public static void main(String[] args) {
+		/*
+		 * Declaramos la variable que la máquina pensará para tratar de adivinar el
+		 * número que piensa el usuario
+		 */
 		int numPensado;
+		// Declaramos la variable para guardar la respuesta del usuario
 		String acierto;
-		int min = 1; 
+		/*
+		 * Declaramos las variables para el valor mínimo y el máximo del intervalo que
+		 * delimitará nuestro número pensado
+		 */
+		int min = 1;
 		int max = 101;
+		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
+		// Explicamos el juego
 		System.out.println("Piensa un número del 1 al 100 y trataré de adivinarlo");
-		System.out.println("Luego me indicas si tu número es 'mayor', 'menor' o 'igual'");
+		// Le pedimos al usuario que pulse INTRO
 		System.out.println("Pulsa INTRO para comenzar");
+		// La consola recogerá la tecla INTRO
 		sc.nextLine();
+		// Creamos un objeto de tipo Random
 		Random r = new Random();
-		
+
 		do {
+			/*
+			 * Asignamos al numPensado un número aleatorio con nuestro mínimo y nuestro
+			 * máximo asignados
+			 */
 			numPensado = r.nextInt(min, max);
-			System.out.println("¿Es el " + numPensado + "?");
-			acierto = sc.nextLine();
-				if (acierto.equalsIgnoreCase("mayor")) {
-					min = numPensado + 1;
-				} else if (acierto.equalsIgnoreCase("menor")) {
-					max = numPensado;
-				} else if (!acierto.equalsIgnoreCase("igual"))
-				System.out.println("Recuerda, escribe 'mayor', 'menor' o 'igual'");
-		} while (!acierto.equalsIgnoreCase("igual"));
-		System.out.println("¡Qué bien!");
+			
+			do {
+				// Mostramos el número aleatorio que se ha generado
+				System.out.println("¿Es el " + numPensado + "? escribe 'mayor', menor' o 'igual'");
+				// Le pedimos al usuario su respuesta
+				acierto = sc.nextLine();
+			} while (!(acierto.equalsIgnoreCase("mayor") || acierto.equalsIgnoreCase("menor") || acierto
+					.equalsIgnoreCase("igual"))); /*
+													 * Si la respuesta no es "mayor", "menor" o "igual"; repetiremos de
+													 * nuevo las instrucciones de dentro de este do
+													 */
+			// Si la respuesta es "mayor", asignaremos al mínimo el numPensado más 1
+			if (acierto.equalsIgnoreCase("mayor"))
+				min = numPensado + 1;
+			else 
+				max = numPensado;
+		} while (!acierto.equalsIgnoreCase("igual") && min != max);
+		System.out.print("¡Qué bien!");
+		if (min == max)
+			System.out.println("... has intentado engañarme... o bueno, puedes haberte equivocado tú");
 		sc.close();
 	}
 }
